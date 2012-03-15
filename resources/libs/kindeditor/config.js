b3ac7b590a339280843f585debc61dbc307b2500
Dong Chen <cdcchen@gmail.com>
2012-03-15 17:57:20 +0800

    disabled flash and media upload access.

--- resources/libs/kindeditor/config.js
+++ resources/libs/kindeditor/config.js
@@ -1,3 +1,42 @@
+var summaryHtmlTags = {
+    a: ['href', 'target', 'name'],
+    embed: ['src', 'width', 'height', 'type', 'loop', 'autostart', 'quality', '.width', '.height', 'align', 'allowscriptaccess'],
+    img: ['src', 'width', 'height', 'border', 'alt', 'title', '.width', '.height'],
+    'div,p,strong,b,sub,sup,em,i,u,strike': []
+};
+
+var contentHtmlTags = {
+    span: [
+            '.color', '.background-color', '.font-size', '.font-family', '.background',
+            '.font-weight', '.font-style', '.text-decoration', '.vertical-align', '.line-height'
+    ],
+    div: [
+            'align', '.border', '.margin', '.padding', '.text-align', '.color',
+            '.background-color', '.font-size', '.font-family', '.font-weight', '.background',
+            '.font-style', '.text-decoration', '.vertical-align', '.margin-left'
+    ],
+    table: [
+            'border', 'cellspacing', 'cellpadding', 'width', 'height', 'align', 'bordercolor',
+            '.padding', '.margin', '.border', 'bgcolor', '.text-align', '.color', '.background-color',
+            '.font-size', '.font-family', '.font-weight', '.font-style', '.text-decoration', '.background',
+            '.width', '.height'
+    ],
+    'td,th': [
+            'align', 'valign', 'width', 'height', 'colspan', 'rowspan', 'bgcolor',
+            '.text-align', '.color', '.background-color', '.font-size', '.font-family', '.font-weight',
+            '.font-style', '.text-decoration', '.vertical-align', '.background'
+    ],
+    a: ['href', 'target', 'name'],
+    embed: ['src', 'width', 'height', 'type', 'loop', 'autostart', 'quality', '.width', '.height', 'align', 'allowscriptaccess'],
+    img: ['src', 'width', 'height', 'border', 'alt', 'title', '.width', '.height'],
+    'p,ol,ul,li,blockquote,h1,h2,h3,h4,h5,h6': [
+            'align', '.text-align', '.color', '.background-color', '.font-size', '.font-family', '.background',
+            '.font-weight', '.font-style', '.text-decoration', '.vertical-align', '.text-indent', '.margin-left'
+    ],
+    pre: ['class'],
+    'hr,br,tbody,tr,strong,b,sub,sup,em,i,u,strike': []
+};
+
 var KEConfig = {
 	mini: {
 		height:180,
@@ -5,6 +44,10 @@ var KEConfig = {
 		allowFileManager: false,
 		allowUpload: true,
 		filterMode: true,
+		allowImageUpload: true,
+		allowFlashUpload: false,
+		allowMediaUpload: false,
+		htmlTags: summaryHtmlTags,
 		bodyClass: 'beta-ke-content beta-post-content',
 		items: ['undo', 'redo', '|', 'plainpaste', 'wordpaste', 'fontname', 'fontsize', 'forecolor', 'hilitecolor', 'insertorderedlist',
 	        'insertunorderedlist', 'bold', 'italic', 'underline', 'strikethrough', '|', 'image', 'flash', 'media',
@@ -16,6 +59,10 @@ var KEConfig = {
 		allowFileManager: false,
 		allowUpload: true,
 		filterMode: true,
+		allowImageUpload: true,
+		allowFlashUpload: false,
+		allowMediaUpload: false,
+		htmlTags: contentHtmlTags,
 		bodyClass: 'beta-ke-content beta-post-content',
 		items: ['undo', 'redo', '|', 'cut', 'copy', 'paste', 'plainpaste', 'wordpaste',
 	        '|', 'justifyleft', 'justifycenter', 'justifyright', 'justifyfull', 'insertorderedlist', 'insertunorderedlist', 'indent', 'outdent', 'subscript', 'superscript', '|', 'link', 'unlink', 'selectall', 'clearhtml', 'removeformat', 'quickformat',
@@ -28,6 +75,10 @@ var KEConfig = {
 		allowFileManager: false,
 		allowUpload: true,
 		filterMode: true,
+		allowImageUpload: true,
+		allowFlashUpload: true,
+		allowMediaUpload: true,
+		htmlTags: summaryHtmlTags,
 		bodyClass: 'beta-ke-content beta-post-content',
 		items: ['undo', 'redo', '|', 'plainpaste', 'wordpaste', 'fontname', 'fontsize', 'forecolor', 'hilitecolor', 'insertorderedlist',
 	        'insertunorderedlist', 'bold', 'italic', 'underline', 'strikethrough',
@@ -43,14 +94,15 @@ var KEConfig = {
 		allowImageUpload: true,
 		allowFlashUpload: true,
 		allowMediaUpload: true,
+		htmlTags: contentHtmlTags,
 		bodyClass: 'beta-ke-content beta-post-content',
 		uploadJson: 'javascript:void(0)',
 		items: ['undo', 'redo', '|', 'cut', 'copy', 'paste', 'plainpaste', 'wordpaste',
 	        '|', 'justifyleft', 'justifycenter', 'justifyright', 'justifyfull', 'insertorderedlist', 'insertunorderedlist', 'indent', 'outdent', 'subscript', 'superscript',
 	        '|', 'forecolor', 'hilitecolor', 'bold', 'italic', 'underline', 'strikethrough', '/',
-	        '|', 'image', 'flash', 'media', 'table', 'hr', 'emoticons', 'link', 'unlink',
-	        '|', 'map', 'code', 'pagebreak', '|', 'fullscreen', 'preview', 'selectall', 'clearhtml', 'removeformat', 'quickformat',
-	        '|', 'source']
+	        '|', 'image', 'flash', 'media', 'table', 'hr', 'emoticons',
+	        '|', 'map', 'code', 'pagebreak', '|', 'link', 'unlink', 'selectall', 'clearhtml', 'removeformat', 'quickformat',
+	        '|', 'fullscreen', 'preview', 'source']
 
 	}
 };
