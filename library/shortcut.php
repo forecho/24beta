275d6ec9ce599b65329140930bdcf6da6e0a9042
Dong Chen <chendong@e23.cn>
2012-04-06 15:19:15 +0800

    eee

--- library/shortcut.php
+++ library/shortcut.php
@@ -4,6 +4,9 @@
  * @version v1.0
  * @since 2010-9-7 10:39
  */
+
+defined('CD_LIBRARY_ROOT') or define('CD_LIBRARY_ROOT', dirname(__FILE__));
+
 /**
  * This is the shortcut to DIRECTORY_SEPARATOR
  */
