1bea12c4c743f5f143631b10c67763a936ae0165
Dong Chen <chendong@e23.cn>
2012-05-18 11:12:30 +0800

    fggfg

--- protected/modules/admin/views/system/error.php
+++ protected/modules/admin/views/system/error.php
@@ -1,4 +1,4 @@
-<div class="alert alert-block">
+<div class="alert alert-block alert-error">
     <h4 class="alert-heading">Error:&nbsp;<?php echo $code;?></h4>
     <p><?php echo $message;?></p>
 </div>
